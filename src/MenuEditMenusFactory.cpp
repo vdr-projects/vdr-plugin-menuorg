281c6f965a540efda4ba98044f136e1deb11389f
svntcreutz <svntcreutz@cd0d6b48-d4f9-0310-940f-ab8c4eb44d3f>
2007-09-13 21:02:55 +0000

    more id properties set
    git-svn-id: file:///home/tobias/sandbox/vdr/--/vdr-pkg/vdr-pkg/menuorg/trunk@6149 cd0d6b48-d4f9-0310-940f-ab8c4eb44d3f

--- src/MenuEditMenusFactory.cpp
+++ src/MenuEditMenusFactory.cpp
@@ -16,7 +16,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * $Id: childlock.cpp 5951 2007-08-24 05:45:20Z svntcreutz $
+ * $Id$
  *
  */
 
--- src/XmlConfigurationFactory.cpp
+++ src/XmlConfigurationFactory.cpp
@@ -16,7 +16,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * $Id: commandmenunode.h 6123 2007-09-04 21:31:19Z svntcreutz $
+ * $Id$
  *
  */
 
